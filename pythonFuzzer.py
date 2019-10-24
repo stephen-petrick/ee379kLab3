6a39f3e63572786570f1756016a54af5a88cf672
stephen-petrick <stephenp1912@gmail.com>
2019-10-24 01:25:21 -0500

    initial push

---
+++ pythonFuzzer.py
@@ -0,0 +1,53 @@
+import sys, socket, ipaddress
+from fuzzbang.alphanumericfuzzer import AlphaNumericFuzzer
+
+def generate_input(n):
+    """
+    Returns an alphanumeric string with a length no greater than n.
+    """
+    fuzzer = AlphaNumericFuzzer(0, n)
+    
+    return fuzzer.generate()
+
+if _name__ == "__main__":
+    # usage
+	"""
+    if len(sys.argv) != 3:
+        print("usage: python3 fuzztut.py num_cases max_length")
+        exit(1)
+	"""        
+   
+ # command-line arguments    
+	num_cases = int(sys.argv[1]) # number of test cases to run
+	max_length = int(sys.argv[2]) # maximum length of each string
+	instruction = sys.argv[3] #particular instruciton of server we're fuzzing
+       
+	results = [] # list for storing the result of each test
+	target = ipaddress.ip_address('10.0.2.4')
+    
+    # main loop
+	for i in range(num_cases):
+
+	
+		s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		#s.settimeout(2) #runs code every 2 seconds, i think
+		s.connect((target,9999)) #hard code server IP address
+		s.recv(9999)
+
+		input = generate_input(max_length) # generate input string of max length specified at cmdline
+		s.send(input)
+		return_value = s.recv() # run name with our input
+        
+		# save test results to our global results list
+		test_result = {}
+		test_result["num"] = i
+		test_result["input"] = input
+		test_result["output"] = return_value
+		results.append(test_result)
+
+	# print summary
+	for test in results:
+		print("Case #{:d}:".format(test["num"]))
+		print("    IN: " + test["input"])
+		print("    OUT: {:4d}".format(test["output"]))
+		print("\n")
